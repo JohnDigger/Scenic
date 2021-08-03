33ef71b1c9c8a0be67574cc1453f7dc76a1ee61b
JohnDigger <541554971@qq.com>
2021-08-04 00:09:12 +0800

    更新

--- src/main/java/com/example/controller/ScenicController.java
+++ src/main/java/com/example/controller/ScenicController.java
@@ -247,6 +247,8 @@ public class ScenicController {
                 jsonResult.setData("文件为空");
                 return jsonResult;
             }
+
+
             // 获取文件名
             String fileName = Objects.requireNonNull(file.getOriginalFilename()).replace(" ","");
             fileName = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + "_" + fileName;
